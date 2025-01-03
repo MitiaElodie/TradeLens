6955f69d41557bc0cd3c108d76950056c035ee04
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2025-01-03 10:21:16 -0500

    The change left

--- TradeLens.Server/obj/Debug/net9.0/TradeLens.Server.AssemblyInfo.cs
+++ TradeLens.Server/obj/Debug/net9.0/TradeLens.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradeLens.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8a4614ad6644be228a14f0922b6de5f9e5f952")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eba954996c5a0f4c565396211a153baa28d00365")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradeLens.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradeLens.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
