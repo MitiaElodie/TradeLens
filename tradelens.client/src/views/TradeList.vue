d8f92ef47eebab1d6e8a45a31fe5d1d3a796ceab
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2025-01-03 10:20:33 -0500

    JS + HTML: JS first and add getTrades

--- tradelens.client/src/views/TradeList.vue
+++ tradelens.client/src/views/TradeList.vue
@@ -1,36 +1,6 @@
-<template>
-   <div class="weather-component">
-       <h1>Weather forecast</h1>
-       <p>This component demonstrates fetching data from the server.</p>
-
-       <div v-if="loading" class="loading">
-           Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationvue">https://aka.ms/jspsintegrationvue</a> for more details.
-       </div>
-
-       <div v-if="post" class="content">
-           <table>
-               <thead>
-                   <tr>
-                       <th>Date</th>
-                       <th>Temp. (C)</th>
-                       <th>Temp. (F)</th>
-                       <th>Summary</th>
-                   </tr>
-               </thead>
-               <tbody>
-                   <tr v-for="forecast in post" :key="forecast.date">
-                       <td>{{ forecast.date }}</td>
-                       <td>{{ forecast.temperatureC }}</td>
-                       <td>{{ forecast.temperatureF }}</td>
-                       <td>{{ forecast.summary }}</td>
-                   </tr>
-               </tbody>
-           </table>
-       </div>
-   </div>
-</template>
 
 <script lang="ts">
+    import { getTrades } from '@/api/tradeLensApi';
    import { defineComponent } from 'vue';
 
    type Forecasts = {
@@ -66,14 +36,43 @@
                this.post = null;
                this.loading = true;
 
-               fetch('weatherforecast')
-                   .then(r => r.json())
-                   .then(json => {
-                       this.post = json as Forecasts;
-                       this.loading = false;
-                       return;
-                   });
+               getTrades()
+                   .then(r => {
+                    console.log(r)
+                   })
            }
        },
    });
 </script>
+
+<template>
+   <div class="weather-component">
+       <h1>Weather forecast</h1>
+       <p>This component demonstrates fetching data from the server.</p>
+
+       <div v-if="loading" class="loading">
+           Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationvue">https://aka.ms/jspsintegrationvue</a> for more details.
+       </div>
+
+       <div v-if="post" class="content">
+           <table>
+               <thead>
+                   <tr>
+                       <th>Date</th>
+                       <th>Temp. (C)</th>
+                       <th>Temp. (F)</th>
+                       <th>Summary</th>
+                   </tr>
+               </thead>
+               <tbody>
+                   <tr v-for="forecast in post" :key="forecast.date">
+                       <td>{{ forecast.date }}</td>
+                       <td>{{ forecast.temperatureC }}</td>
+                       <td>{{ forecast.temperatureF }}</td>
+                       <td>{{ forecast.summary }}</td>
+                   </tr>
+               </tbody>
+           </table>
+       </div>
+   </div>
+</template>
