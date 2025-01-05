a62482d785f940a605c9215345ecbd41ac7f76d0
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2025-01-04 19:39:35 -0500

    JS: set the right api

--- tradelens.client/src/api/tradeLensApi.ts
+++ tradelens.client/src/api/tradeLensApi.ts
@@ -1,6 +1,6 @@
 import axiosInstance from "@/plugins/axios";
 
 export function getTrades() {
-    return axiosInstance.get("/WeatherForecast")
+    return axiosInstance.get("/api/v1/trades")
         .then(response => response.data);
 }
