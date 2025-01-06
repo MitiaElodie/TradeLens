45f7065562602a5b18f6161b62ec2e10f9f420c3
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2025-01-05 19:45:21 -0500

    JS + HTML: display the trade list with v-data-table

--- tradelens.client/src/views/TradeList.vue
+++ tradelens.client/src/views/TradeList.vue
@@ -1,53 +1,43 @@
 
 <script lang="ts">
-    import { getTrades } from '@/api/tradeLensApi';
-   import { defineComponent } from 'vue';
+import { getTrades } from '@/api/tradeLensApi';
+import { TRADE_LIST_HEADER } from '@/constants.js';
+export default {
+   data() {
+      return {
+         loading: false,
+         trades: [],
+         TRADE_LIST_HEADER,
+      };
+   },
 
-   type Forecasts = {
-       date: string,
-       temperatureC: string,
-       temperatureF: string,
-       summary: string
-   }[];
+   created() {
+      this.fetchData();
+   },
 
-   interface Data {
-       loading: boolean,
-       post: null | Forecasts
-   }
+   methods: {
+      fetchData(): void {
+         this.loading = true;
 
-   export default defineComponent({
-       data(): Data {
-           return {
-               loading: false,
-               post: null
-           };
-       },
-       created() {
-           // fetch the data when the view is created and the data is
-           // already being observed
-           this.fetchData();
-       },
-       watch: {
-           // call again the method if the route changes
-           '$route': 'fetchData'
-       },
-       methods: {
-           fetchData(): void {
-               this.post = null;
-               this.loading = true;
-
-               getTrades()
-                   .then(r => {
-                    console.log(r)
-                   })
-           }
-       },
-   });
+         getTrades()
+            .then(trades => {
+               console.log(trades)
+               this.trades = trades;
+            })
+            .finally(() => {
+               this.loading = false;
+            });
+      }
+   },
+};
 </script>
 
 <template>
-   <div class="trade-list">
-       <h1 class="trade-list__title">Trade journal</h1>
-        <v-btn>Vuetify Button</v-btn>
-   </div>
+<div class="trade-list">
+   <h1 class="trade-list__title">Trade journal</h1>
+   <v-data-table
+      :headers="TRADE_LIST_HEADER"
+      :items="trades"
+   ></v-data-table>
+</div>
 </template>
