2d33156b3265649e62ac18ad919db8f93bab3361
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2025-01-06 20:48:56 -0500

    JS + HTML: init multiselectTradeType

---
+++ tradelens.client/src/components/multiselect/MultiselectTradeType.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { TRADE_TYPES } from '@/constants.js';
+export default {
+   name: 'MultiselectTradeType',
+
+   props: {
+      value: {
+         type: String,
+         required: true,
+      },
+   },
+
+   computed: {
+      tradeTypes(): string[] {
+         return Object.values(TRADE_TYPES).map(type => type.name);
+      },
+   },
+}
+</script>
+<template>
+   <v-autocomplete
+      label="Type"
+      :items="tradeTypes"
+   ></v-autocomplete>
+</template>
