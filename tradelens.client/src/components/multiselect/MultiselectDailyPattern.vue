f7a643bb0f0e2c0417276f4d3cd5bb2cb0bf9225
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2025-01-07 21:34:08 -0500

    JS + HTML: add MultiselectDailyPattern

---
+++ tradelens.client/src/components/multiselect/MultiselectDailyPattern.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { DAILY_PATTERN } from '@/constants.js';
+export default {
+   name: 'MultiselectDailyPattern',
+
+   props: {
+      value: {
+         type: String,
+         required: true,
+      },
+   },
+
+   computed: {
+      dailyPatterns(): object[] {
+         return Object.values(DAILY_PATTERN);
+      },
+   },
+}
+</script>
+<template>
+   <v-autocomplete
+      label="Daily pattern"
+      :items="dailyPatterns"
+      item-title="label"
+   ></v-autocomplete>
+</template>
