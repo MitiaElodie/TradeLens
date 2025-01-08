933c689b85d673803b26e03391ed7dc7b00ebb16
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2025-01-08 14:57:43 -0500

    JS: create function getEmptyTrade

--- tradelens.client/src/utils.ts
+++ tradelens.client/src/utils.ts
@@ -1,9 +1,28 @@
 import { TRADE_TYPES } from "@/constants.js";
 
 export function getTradePrefix(type: string): string {
-    return TRADE_TYPES[type].prefix;
+   return TRADE_TYPES[type].prefix;
 }
 
 export function getTradePrefixedId(id: number, type: string): string {
-    return `${getTradePrefix(type)}-${id}`;
+   return `${getTradePrefix(type)}-${id}`;
+}
+
+export function getEmptyTrade(): object {
+   return {
+      type: '',
+      pair: '',
+      result: '',
+      weeklyLastCandleClose: false,
+      weeklySupplyAndDemand: false,
+      weeklyMarketStructure: false,
+      weeklyFibonacci: false,
+      dailyPattern: '',
+      h4Pattern: '',
+      weeklyScreenshot: '',
+      dailyScreenshot: '',
+      h4Screenshot: '',
+      missed: false,
+      note: '',
+   }
 }
