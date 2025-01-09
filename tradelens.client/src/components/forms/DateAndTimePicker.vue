07e6a525a4a8a24e1115ea4e3663c180f8317b7d
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2025-01-08 20:18:42 -0500

    JS + HTML: init DateAndTimePicker

---
+++ tradelens.client/src/components/forms/DateAndTimePicker.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+import VueDatePicker from '@/plugins/vueDatePicker.ts'
+
+export default {
+   name: 'DateAndTimePicker',
+   components: {
+      VueDatePicker,
+   },
+   props: {
+      modelValue: {
+         type: String,
+         default: '',
+      },
+
+      label: {
+         type: String,
+         default: '',
+      },
+   },
+
+   emits: ['update:modelValue'],
+
+   data() {
+      return {
+         date: '',
+      }
+   },
+
+   created() {
+      this.date = this.modelValue
+   },
+
+   methods: {
+      updateDate(date: string) {
+         this.date = date
+         this.$emit('update:modelValue', date)
+      },
+   },
+}
+</script>
+<template>
+   <div class="date-and-time-picker">
+      <label class="date-and-time-picker__label">{{ label }}</label>
+      <VueDatePicker
+         v-model="date"
+         :label="label"
+         time-picker-inline
+         @update:modelValue="updateDate"
+      />
+   </div>
+</template>
